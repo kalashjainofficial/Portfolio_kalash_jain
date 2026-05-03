2d4630af27b78a83964e3c2884b823a275460243
kalashjainofficial <kalashjainofficial@gmail.com>
2026-05-03 22:49:16 +0530

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Portfolio_kalash_jain/", // Add this line!
   plugins: [
     react(),
     tailwindcss()
