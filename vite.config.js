82b35e1abf294da3a20bb95fec89cd5179ff26d8
kalashjainofficial <kalashjainofficial@gmail.com>
2026-05-04 17:14:49 +0530

    for netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio_kalash_jain/", 
+
   plugins: [
     react(),
     tailwindcss()
